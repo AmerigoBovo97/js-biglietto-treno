973f6b632558438ee4034ea951d0d20384db5839
AmerigoBovo97 <amerigobovo@gamil.com>
2024-10-07 19:36:49 +0200

    feat: add age prompt

--- js/main.js
+++ js/main.js
@@ -15,5 +15,26 @@ while (running)
         distance = prompt("Non hai inserito un numero");
     }
 
-}
-console.log(distance)
+};
+
+
+let age = prompt("Quanti anni hai?");
+
+running = true;
+while (running)
+{
+    if (!isNaN(age)){
+        age = Number(age)
+        if (age >= 0 && age <= 150){
+            const total = age * 0.21;
+            running =  false;
+        }else{
+            age = prompt("Il numero di km deve essere compreso tra 0 e 150");
+        };
+    }else{
+        age = prompt("Non hai inserito un numero");
+    }
+
+};
+
+console.log(age)
